6fcfd3e82f5befe8ea660035ab6f5acfa77e848c
dihhh <toppatclan01@gmail.com>
2026-03-13 09:16:16 +0700

    added user&notification api + cloudinary

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema(
         password:{
             type: String,
             required: true,
-            minLength: 6,
+            minLength: 7,
         },
         email:{
             type: String,
