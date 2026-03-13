f0274398ad84a086b04977b1e0c6d864d80a0676
dihhh <toppatclan01@gmail.com>
2026-03-13 09:25:43 +0700

    updateuser bug fix at let and fullname

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -89,13 +89,13 @@ export const getSuggestedUsers = async (req, res) => {
 }
 
 export const updateUser = async (req, res) => {
-	const { fullName, email, username, currentPassword, newPassword, bio, link } = req.body;
+	const { fullname, email, username, currentPassword, newPassword, bio, link } = req.body;
 	let {profileImg,coverImg} = req.body;
 
 	const userId = req.user._id;
 
 	try {
-		const user = await User.findById(userId);
+		let user = await User.findById(userId);
 		if(!user) return res.status(404).json({ message: "User not found"});
 	
 		if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
@@ -146,6 +146,7 @@ export const updateUser = async (req, res) => {
 
 		return res.status(200).json(user);
 	} catch (error) {
-		
+		console.log("Error in updateUser: ", error.message);
+		res.status(500).json({ error: error.message });
 	}
 };
